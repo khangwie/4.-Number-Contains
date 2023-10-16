c7930ff94d82623ecaa4937e35e498864038a7bf
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-16 12:18:39 +0200

    Add project files.

---
+++ 4. Number Contains/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Number_Contains
+{
+    internal class Program
+    {
+        static bool IfNumberContains3(int number)
+        {
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit == 3)
+                    return true;
+
+                number /= 10;
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(); // calling nethods whether a number contains 3 digit or Console.MriteLine("Is the number contains 3 digit");
+            Console.WriteLine("Is the number contains 3 digits");
+            //int nun = 7201432;
+
+            int num = 87501;
+            bool contains3 = IfNumberContains3(num);
+            if (contains3)
+            {
+                Console.WriteLine($"The number does not contain ({num}) -> {contains3}");
+            }
+            else
+            {
+                Console.WriteLine($"The number does not contain ({num}) -> {contains3}");
+                Console.ReadLine();
+            }
+        }
+         
+    
+    }
+}
